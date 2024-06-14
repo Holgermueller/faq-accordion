12359d68a7fde0c8708f8bf297a832abfde0d1c4
holgermueller <9jholger@gmail.com>
2024-06-14 15:02:08 -0400

    began desktop layout

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ let acc = document.getElementsByClassName("accordion-trigger");
 
 for (let i = 0; acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+    console.log("click" + i);
   });
 }
