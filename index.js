5ea3afa1def4b660530b4d32af4c961bfe593dbc
holgermueller <9jholger@gmail.com>
2024-06-14 13:51:24 -0400

    styled mobile display

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let acc = document.getElementsByClassName("accordion-trigger");
+
+for (let i = 0; acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
