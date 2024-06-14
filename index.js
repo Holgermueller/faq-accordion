d28aed99fc1e8d7b6c52026003899733e80ffa92
holgermueller <9jholger@gmail.com>
2024-06-14 17:42:47 -0400

    got accrodion to open and close

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 "use strict";
 
-let acc = document.getElementsByClassName("accordion-trigger");
+let acc = document.getElementsByClassName("panel-trigger");
 
-for (let i = 0; acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    console.log("click" + i);
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      console.log("close");
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      console.log("open");
+    }
   });
 }
