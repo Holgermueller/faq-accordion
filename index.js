611904825d7fa75d9e70bf0859b4e6a4e85ad83f
holgermueller <9jholger@gmail.com>
2024-06-14 22:46:16 -0400

    pages loads with first accordion open

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 "use strict";
 
 let acc = document.getElementsByClassName("panel-trigger");
+let i;
 
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function () {
     this.classList.toggle("active");
     let panel = this.nextElementSibling;
     let img = document.body.querySelector(".accordion-icon");
@@ -12,5 +13,6 @@ for (let i = 0; i < acc.length; i++) {
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
-  });
+  };
 }
+acc[0].onclick();
